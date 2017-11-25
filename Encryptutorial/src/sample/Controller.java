8522fb19fa0af9fc9849cdcc4478a3a8b7a09e72
Rachel Popo <Rach.Popo@gmail.com>
2017-11-25 15:28:43 -0700

    Updated

--- Encryptutorial/src/sample/Controller.java
+++ Encryptutorial/src/sample/Controller.java
@@ -92,7 +92,7 @@ public class Controller implements Initializable {
 
     public void initialize(URL fxmlFileLocation, ResourceBundle resources) {
 
-        promptText.setText("Welcome to the Simulated Encrypted Chat program, an educational tool for understanding encryption! Please enter your name below!");
+        promptText.setText("Welcome to the Simulated Encrypted Chat program, an educational tool for understanding encryption! \n\n Please enter your name below.");
 
         nextButton.setOnAction(e -> {
                 runStep(step);
@@ -135,24 +135,24 @@ public class Controller implements Initializable {
         String username;
         username = entryText.getText();
         nameText.setText(username + " !");
-        promptText.setText("Hi, " + username + "!  \n" + "Encryption is the process of encoding information so only authorized parties can access it. Encryption does NOT prevent your message from being intercepted by the privacy invaders of the Internet. However, encryption does ensure that only the intended recipient(s) will be able to decode your message.");
+        promptText.setText("Hi, " + username + "!  \n\n" + "Encryption is the process of encoding information so only authorized parties can access it. Encryption does NOT prevent your message from being intercepted by the privacy invaders of the Internet. However, encryption does ensure that only the intended recipient(s) will be able to decode your message.");
         entryText.setText("");
     }
 
     private void Step1(){
-        promptText.setText("As a dedicated protector of privacy, this program strives to deliver effective chat encryption so information is only accessible by intended recipients.  Let’s get started!");
+        promptText.setText("As a dedicated protector of privacy, this program strives to deliver effective chat encryption so information is only accessible by intended recipients. \n\n Let’s get started!");
     }
 
     private void Step2(){
-        promptText.setText("To encrypt anything, a key must be used alongside an encryption algorithm to encode and decode the information.\n" + "\n" +
-                "Symmetric encryption is a simple method, that requires a single key for encrypting and decrypting data. \n" + "\n" +
+        promptText.setText("To encrypt anything, a key must be used alongside an encryption algorithm to encode and decode the information.\n\n" +
+                "Symmetric encryption is a simple method, that requires a single key for encrypting and decrypting data. \n\n" +
                 "Asymmetric encryption is a more secure method and requires different keys for encrypting and decrypting data.");
     }
 
     private void Step3(){
-        promptText.setText("Since this program aims to provide the highest security, we use asymmetric encryption.\n" +
-                "A key pair is generated for all our users, containing:\n" +
-                "A public key, used to encrypt. You share this key with other users so they can send you encrypted messages.\n" +
+        promptText.setText("Since this program aims to provide the highest security, we use asymmetric encryption.\n\n" +
+                "A key pair is generated for all our users, containing:\n\n" +
+                "A public key, used to encrypt. You share this key with other users so they can send you encrypted messages.\n\n" +
                 "A private key, used to decrypt. ONLY YOU should have this key. If someone else gets access to your private key, they can decrypt messages that were meant for your eyes only.\n" +
                 "\n" +
                 "Let’s generate your key pair, now!\n");
@@ -180,25 +180,23 @@ public class Controller implements Initializable {
         String bPub = Benc.publickey;
         bPriText.setText(bPri);
         bPubText.setText(bPub);
-        
+
         promptText.setText("Your RSA key pair has been generated! Take a look!" +
                 "\n" +
-                "Just for convenience, we'll show our buddy Bob's keys as well!");
+                "For learning purposes, we'll show Bob's keys as well!");
 
     }
 
     private void Step5(){
-        promptText.setText("Let’s send a message to Alice's good ‘ol friend, Bob!" +
-                "\n" +
+        promptText.setText("Let’s send a message to your good ‘ol friend, Bob!" +
+                "\n\n" +
                 "Enter your message below.");
-        bPriText.setText("give me a key!");
-        bPubText.setText("me too!");
     }
 
     private void Step6(){
         messSent.setText(entryText.getText());
         entryText.setText("");
-        promptText.setText("To ensure nobody but Bob can read the contents of your message, let’s encrypt it!\n" +
+        promptText.setText("To ensure nobody but Bob can read the contents of your message, let’s encrypt it!\n\n" +
                 "Which key are we going to encrypt with?\n" +
                 "a. Bob’s public key\n" +
                 "b. Bob’s private key\n" +
@@ -210,13 +208,13 @@ public class Controller implements Initializable {
         answers = entryText.getText();
         if( answers.equals("a") || answers.equals("a.") || answers.equals("A") || answers.equals("A."))
         {
-            promptText.setText("Correct! The message should be encrypted using Bob’s public key.\n" +
+            promptText.setText("Correct! The message should be encrypted using Bob’s public key.\n\n" +
                     "Then the message can only be decrypted with Bob’s private key, which only Bob should have.\n" +
                     "This ensures only Bob will be able to decode your message. \n");
             entryText.setText("");
         } else
         {
-            promptText.setText("Not quite! The message should be encrypted using Bob’s public key.\n" +
+            promptText.setText("Not quite! The message should be encrypted using Bob’s public key.\n\n" +
                     "Then the message can only be decrypted with Bob’s private key, which only Bob should have.\n" +
                     "This ensures only Bob will be able to decode your message. \n");
             entryText.setText("");
@@ -236,7 +234,7 @@ public class Controller implements Initializable {
     }
 
     private void Step10(){
-        promptText.setText("Your message has been sent! While we’re waiting for a reply.. \n" +
+        promptText.setText("Your message has been sent! While we’re waiting for a reply.. \n\n" +
                 "Which key will Bob use to decrypt your message?\n" +
                 "a. Bob’s public key\n" +
                 "b. Bob’s private key\n" +
