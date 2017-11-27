b5fdb041f89179a30d8e542fa0655b033647492a
Paul <paul.brown@ucdenver.edu>
2017-11-26 20:58:42 -0700

    cleaned house a little bit and added variable comments

--- Encryptutorial/src/sample/Encrypt.java
+++ Encryptutorial/src/sample/Encrypt.java
@@ -14,10 +14,9 @@ public class Encrypt {
      String publickey;
      String privatekey;
      Cipher cipher;
-     byte[] cipherText;
+     byte[] cipherText; //encrypted message stored in byte format
 
     public Encrypt(){}
-
 
     // function to encrypt a message
     // pass public key to encrypt with and message to encrypt
@@ -33,7 +32,6 @@ public class Encrypt {
 
     }
 
-
     // function to decrypt a message
     // pass private key to decrypt with and message to decrypt
     public String decrypt(byte[] message, KeyPair pk) throws Exception{
@@ -48,11 +46,10 @@ public class Encrypt {
     // generate keypair
     // returns new key pair
     public KeyPair generateRSA() throws Exception{
+
         KeyPairGenerator keyGen = KeyPairGenerator.getInstance("RSA");
         keyGen.initialize(1024);
         KeyPair key = keyGen.generateKeyPair();
-        //publickey = new String(key.getPublic().getEncoded());
-        //privatekey = new String(key.getPrivate().getEncoded());
 
         Base64.Encoder encoder = Base64.getEncoder();
         privatekey = encoder.encodeToString(key.getPrivate().getEncoded());
@@ -62,51 +59,19 @@ public class Encrypt {
 
     }
 
-
     // get public key from a KeyPair object
     // return public key encoded as string
     public String publicToString(KeyPair keypair){
+
         Base64.Encoder encoder = Base64.getEncoder();
         return encoder.encodeToString(keypair.getPublic().getEncoded());
     }
 
-
     // get private key from a KeyPair object
     // return private key encoded as string
     public String privateToString(KeyPair keypair){
+
         Base64.Encoder encoder = Base64.getEncoder();
         return encoder.encodeToString(keypair.getPrivate().getEncoded());
-    }
-
-
-    public void message(String input) throws Exception {
-
-
-        byte[] plainText = input.getBytes("UTF8");
-        //
-        // generate an RSA key
-        System.out.println("\nStart generating RSA key");
-        KeyPairGenerator keyGen = KeyPairGenerator.getInstance("RSA");
-        keyGen.initialize(1024);
-        KeyPair key = keyGen.generateKeyPair();
-        System.out.println("Finish generating RSA key");
-        //
-        // get an RSA cipher object and print the provider
-        Cipher cipher = Cipher.getInstance("RSA/ECB/PKCS1Padding");
-        System.out.println("\n" + cipher.getProvider().getInfo());
-        //
-        // encrypt the plaintext using the public key
-        System.out.println("\nStart encryption");
-        cipher.init(Cipher.ENCRYPT_MODE, key.getPublic());
-        byte[] cipherText = cipher.doFinal(plainText);
-        System.out.println("Finish encryption: ");
-        System.out.println(new String(cipherText, "UTF8"));
-        //
-        // decrypt the ciphertext using the private key
-        System.out.println("\nStart decryption");
-        cipher.init(Cipher.DECRYPT_MODE, key.getPrivate());
-        byte[] newPlainText = cipher.doFinal(cipherText);
-        System.out.println("Finish decryption: ");
-        System.out.println(new String(newPlainText, "UTF8"));
     }
 }
